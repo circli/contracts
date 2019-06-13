a2ff40a3ddca1cb3bdbf619a68bba19c644932df
Andreas Sundqvist <andreas@forme.se>
2019-06-13 21:21:06 +0200

    Remove deprecated interfaces

--- src/EventProviderInterface.php
+++
@@ -1,8 +0,0 @@
-<?php declare(strict_types=1);
-
-namespace Circli\Contracts;
-
-interface EventProviderInterface
-{
-	public function registerEvents(\Psr\EventDispatcher\EventDispatcherInterface $dispatcher): void;
-}
--- src/EventSubscriberInterface.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace Circli\Contracts;
-
-interface EventSubscriberInterface
-{
-	public static function getSubscribedEvents(): array;
-}
